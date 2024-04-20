4a6519df3d51904ac9ae02d526312749f3dc949f
nkmr <tomnkmr0120@gmail.com>
2024-04-20 13:51:19 +0900

    LoginFormの削除

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()  # こっちで先に変数代入する！
 
@@ -13,13 +13,3 @@ class SignupForm(UserCreationForm):
 # password1, password2というフィールドはUserCreationFormの方で設定されているため、
 # fieldsの欄には、Userモデルの中にある、
 # blankにはできない値であるusernameとemailをセットする。
-
-
-class LoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            # 全てのフォームの部品のclass属性に「form-control」を指定（bootstrapのフォームデザインを利用するため）
-            field.widget.attrs["class"] = "form-control"
-            # 全てのフォームの部品にpaceholderを定義して、入力フォームにフォーム名が表示されるように指定。
-            field.widget.attrs["placeholder"] = field.label
