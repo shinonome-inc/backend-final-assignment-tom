4bb71b41db2f243c8654bcd3a9f3c9e71544df65
nkmr <tomnkmr0120@gmail.com>
2024-04-28 11:59:49 +0900

    TweetFormの作成

---
+++ tweets/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Tweet
+
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        field = ['text']
+        labels = {'text': 'Tweet Contents'}
