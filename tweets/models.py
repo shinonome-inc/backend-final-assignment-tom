e75919246b8f05644ce6d97eafd451e919a19d56
nkmr <tomnkmr0120@gmail.com>
2024-04-28 11:59:24 +0900

    Tweetモデルの定義

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,9 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
 
-# class Tweet(models.Model):
+class Tweet(models.Model):
+    # 関連するオブジェクトが削除された場合に、それに関連するすべてのオブジェクトも一緒に削除される
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
