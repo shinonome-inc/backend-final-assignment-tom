95a37e4ca6a41f7761dcb8dec394cb5e3fb9fa98
nkmr <tomnkmr0120@gmail.com>
2024-07-22 23:57:00 +0900

    ダブルクォーテーションに統一

--- accounts/views.py
+++ accounts/views.py
@@ -36,8 +36,8 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     # ログインしているユーザーのツイートのみ取得
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        username = self.kwargs.get('username')
+        username = self.kwargs.get("username")
         user = get_object_or_404(User, username=username)
-        context['user'] = user
+        context["user"] = user
         context["profile_list"] = Tweet.objects.filter(user=self.object).select_related("user")
         return context
